892d48b043f7089fb8c524f2f7d12b627cb3eab2
Kiran Bhatta <bhattakiran1111@gmail.com>
2025-08-12 13:58:16 +0545

    Create first.html
    This repository contains a simple JavaScript script that demonstrates basic browser interactions and DOM manipulation. The script includes:
    -Writing content to the page using document.write()
    -Displaying messages and input prompts using alert(), prompt(), and confirm()
    -Logging to the browser console

---
+++ first.js
@@ -0,0 +1,14 @@
+document.write("This is from JS ");
+document.write("This is also from JS");
+
+alert("Hello");
+console.log("This is displayed in the console!");
+
+let ans = prompt("How are you?");
+alert("Response is: " + ans);
+
+let resp = confirm("Are you sure?");
+alert("Response is: " + resp);
+
+let pa = document.getElementById("myparagraph");
+pa.innerHTML = "<i>This is added by JS</i>";
