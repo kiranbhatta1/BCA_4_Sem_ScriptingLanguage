b72a7c5b26dc49d2ee21c720e3e184f0966cbaef
Kiran Bhatta <bhattakiran1111@gmail.com>
2025-08-12 13:58:41 +0545

    Update first.js

--- first.js
+++ first.js
@@ -1,14 +1,27 @@
-document.write("This is from JS ");
-document.write("This is also from JS");
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JavaScript Example</title>
+</head>
+<body>
+    <p>This is a paragraph</p>
+    <p id="myparagraph">The content is loaded from JS</p>
 
-alert("Hello");
-console.log("This is displayed in the console!");
+    <script>
+        document.write("This is from JS ");
+        document.write("This is also from JS");
 
-let ans = prompt("How are you?");
-alert("Response is: " + ans);
+        alert("Hello");
+        console.log("This is displayed in the console!");
 
-let resp = confirm("Are you sure?");
-alert("Response is: " + resp);
+        let ans = prompt("How are you?");
+        alert("Response is: " + ans);
 
-let pa = document.getElementById("myparagraph");
-pa.innerHTML = "<i>This is added by JS</i>";
+        let resp = confirm("Are you sure?");
+        alert("Response is: " + resp);
+
+        let pa = document.getElementById("myparagraph");
+        pa.innerHTML = "<i>This is added by JS</i>";
+    </script>
+</body>
+</html>
